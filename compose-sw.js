b20f62f463c3738d0c32db3f0402448697de938d
tracefrags <hmzshabs@gmail.com>
2026-08-24 21:46:36 +0500

    Add files via upload

---
+++ compose-sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'trace-compose-v1';
+const FILES = ['./compose-map.html', './compose-manifest.json', './icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).catch(() => {}));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(hit =>
+      hit || fetch(e.request).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+        return res;
+      }).catch(() => hit)
+    )
+  );
+});
